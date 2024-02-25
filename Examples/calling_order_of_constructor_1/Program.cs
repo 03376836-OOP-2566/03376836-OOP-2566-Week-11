ebdb946f7627b6d6b1fd1459871acd94199b187c
koson <567256+koson@users.noreply.github.com>
2024-02-26 06:48:11 +0700

    add examples

---
+++ Examples/calling_order_of_constructor_1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+var derived = new DerivedClass();
+
+
+class BaseClass 
+{
+    public BaseClass()
+    {
+        System.Console.WriteLine("Hello from BaseClass constructor");
+    }
+}
+
+class DerivedClass : BaseClass
+{
+    public DerivedClass()
+    {
+        System.Console.WriteLine("Hello from DerivedClass constructor");
+    } 
+}
+
+
